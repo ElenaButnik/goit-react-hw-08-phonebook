8ebc0fa4136a960d188d1a78c65fe61d557537fe
ElenaButnik <butnik.elena@gmail.com>
2021-12-05 19:30:32 +0200

    исправлена ошибка при логинизации

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { Switch } from 'react-router-dom';
+import { useEffect, Suspense, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import Navigation from './components/Navigation';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import { currentUser } from './redux/auth/auth-operations';
+
+
+const ContactsPage = lazy(() => import('./pages/Contacts'));
+const RegisterPage = lazy(() => import('./pages/Register'));
+const LoginPage = lazy(() => import('./pages/Login'));
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(currentUser());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Navigation />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <PrivateRoute exact path="/" redirectTo="/login">
+            <ContactsPage />
+          </PrivateRoute>
+          <PublicRoute exact path="/register" restricted>
+            <RegisterPage />
+          </PublicRoute>
+          <PublicRoute exact path="/login" restricted>
+            <LoginPage />
+          </PublicRoute>
+        </Switch>
+      </Suspense>
+    </div>
+  );
+}
---
+++ src/components/ContactList.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import style from "./contacts.module.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { getThunkData, deleteThunkData } from '../redux/contacts/operations';
+import { getFilteredContacts } from '../redux/contacts/selectors';
+
+
+export default function ContactList() {
+
+  const dispatch = useDispatch();
+  const filterContacts = useSelector(getFilteredContacts);
+
+  useEffect(() => {
+    dispatch(getThunkData());
+  }, [dispatch]);
+
+  return (
+      <ul className={style.list}>
+      {filterContacts.map((el) => (
+          <li className={style.listItem} key={el.id}>
+            {el.name}: {el.number}
+            <button
+              type="button"
+              className={style.btnDelete}
+              id={el.id}
+            onClick={() => {
+              dispatch(deleteThunkData(el.id));
+              dispatch(getThunkData());
+            }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+}
